36ced03f60fbc838b949120f3783e22cc27a41c1
Avra Mitra <abhra0897@gmail.com>
2022-12-17 06:51:07 +0530

    VirtualGallery: added different texture for wall, added 3d plants, text planes and more

---
+++ README.md
@@ -0,0 +1,5 @@
+## ThreeJS projects
+
+I put my three.js projects here. They are awful in quality, but I do enjoy it.
+
+I am currently working on "virtual gallery" project.
---
+++ test1/src/app.js
@@ -0,0 +1,219 @@
+import * as THREE from './three.module.js';
+import { PointerLockControls } from './PointerLockControls.js';
+
+
+var velocityForward = 2;
+var velocityBackward = 1;
+var velocityLeft = 1;
+var velocityRight = 1;
+
+var canMoveForward = false;
+var canMoveLeft = false;
+var canMoveRight = false;
+var canMoveBackward = false;
+
+var scene = new THREE.Scene();
+//fog creation
+{
+	const color = 0x101010
+	const density = 0.007;
+	scene.fog = new THREE.FogExp2(color, density);
+}
+
+
+// Create Camera
+var camera = new THREE.PerspectiveCamera(65, window.innerWidth / window.innerHeight, 0.1, 2000); 
+camera.position.set(0, 15, 60);
+camera.lookAt(new THREE.Vector3(0, 0, 0));
+
+// Create WebGL Renderer
+var renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+
+//camera control FPS
+var controls = new PointerLockControls(camera, document.body);
+controls.connect();
+
+
+document.body.appendChild(renderer.domElement);
+//movement
+document.body.addEventListener('keydown', onKeyDown, false);
+document.body.addEventListener('keyup', onKeyUp, false);
+//lock cursor
+document.body.addEventListener( 'mousedown', onMouseDown, false );
+
+
+var geometrySphere = new THREE.SphereGeometry();
+var materialSphere = new THREE.MeshBasicMaterial({
+	color: 0x00ff00,
+	wireframe: true
+});
+var sphere = new THREE.Mesh(geometrySphere, materialSphere);
+
+//no of houses on each side
+var house_count = 30;
+//Adding a floor
+var floor_length = ((10 + 2) * house_count);
+var geometryFloor = new THREE.PlaneGeometry(30, floor_length, 20, 40);
+var materialFloor = new THREE.MeshBasicMaterial({ color: 0x00ff00, wireframe: true });
+var floor_plane = new THREE.Mesh(geometryFloor, materialFloor);
+var rad_to_deg_factor = 0.01746032;
+floor_plane.rotation.x = 90 * rad_to_deg_factor;
+floor_plane.position.z = -(floor_length / 2) + 10;
+
+//All houses are packed inside on Object3D
+var cubes = new THREE.Object3D();
+//Houses on left
+draw_houses(house_count, -20);
+scene.add(cubes);
+//Houses on right
+draw_houses(house_count, 20);
+
+scene.add(cubes);
+scene.add(floor_plane);
+
+
+
+//Is camera reached the maximum Z position?
+var is_max_reached = false;
+var camera_automove = true;
+function animate()
+{
+	
+	if (camera_automove === true)
+	{
+		if (camera.position.z < -(floor_length))
+		{
+			is_max_reached = true;
+		}
+			
+		if (camera.position.z >= 60) {
+			is_max_reached = false;
+		}
+
+		if (is_max_reached == true)
+			camera.position.z += 0.2;
+		else
+			camera.position.z -= 0.2;
+	}	
+	handleMovement();
+	requestAnimationFrame(animate);
+	renderer.render(scene, camera);
+}
+
+//house_count: No of houses
+//x_pos: position of boxes on x axis
+function draw_houses(house_count, x_pos)
+{
+
+	var cube_h_min = 20;
+	var cube_h_max = 50;
+	var cube_h = cube_h_min;
+	var cube_w = 15;
+	var cube_d = 10;
+	var cube_pos_z = 0;
+	var cube_pos_y = cube_h / 2;
+
+	//if negarive, left side. Else right side
+	var cube_pos_x = x_pos;
+
+	
+	for (var cnt = 0; cnt < house_count; cnt++)
+	{
+		var geometryCube_auto = new THREE.BoxGeometry(cube_w, cube_h, cube_d, 5, 5, 2);
+		var color_rand = new THREE.Color(0xffffff);
+		color_rand.setHex(Math.random() * 0xffffff);
+
+		var materialCube_rand = new THREE.MeshBasicMaterial({
+			color: color_rand,
+			wireframe: true
+		});
+
+		var cube_auto = new THREE.Mesh(geometryCube_auto, materialCube_rand);
+		cube_auto.position.set(cube_pos_x, cube_pos_y, cube_pos_z);
+		cubes.add(cube_auto);
+
+		cube_pos_z -= cube_d + 2;   //Place cubes behind
+		cube_h = Math.floor(Math.random() * (cube_h_max - cube_h_min)) + cube_h_min;  //range min to max
+		cube_pos_y = cube_h / 2;    //all cube's base must be at y = 0 (so, center = h/2)
+
+		
+	}
+	
+}
+
+
+//Jump
+//Lock cursor on click
+function onMouseDown(event)
+{
+	switch ( event.button ) 
+	{
+		//left click
+		case 0: 
+			//controls.lock();
+			break;
+		
+		//right click
+		case 2: 
+			controls.unlock();
+			break;
+	}
+}
+
+
+//Forward/backword movement
+//esc to unlock cursor
+//enter to lock cursor
+function onKeyDown()
+{
+	switch (event.keyCode)
+	{
+		case 83: // S
+			canMoveBackward = true;
+			break;
+		case 87: // W
+			canMoveForward = true;
+			break;
+		case 27: // space
+			canJump = true;
+		case 27: // esc
+			controls.unlock();
+			break;
+		case 13: // Enter
+			controls.lock();
+			break;
+	}
+}
+
+
+function onKeyUp()
+{
+	switch (event.keyCode)
+	{
+		case 83: // S
+			canMoveBackward = false;
+			break;
+		case 87: // W
+			canMoveForward = false;
+			break;
+		case 27: // space
+			canJump = false;
+	}
+}
+
+
+function handleMovement()
+{
+	if (canMoveForward)
+		controls.moveForward(velocityForward);
+	if (canMoveBackward)
+		controls.moveForward(-velocityBackward);
+	if (canMoveRight)
+		controls.moveRight(velocityRight);
+	if (canMoveLeft)
+		controls.moveRight(-velocityLeft);
+}
+
+//Finally animate
+animate();
